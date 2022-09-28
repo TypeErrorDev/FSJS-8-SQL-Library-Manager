91ca5d0b1d225241a2dad8ccfcfcc11c95523174
TypeErrorDev <matthew.pantel86@gmail.com>
2022-09-29 01:34:14 +0200

    ADD: completed the newBook route

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const Book = require("../models").Book;
 //      GET Requests       //
 /////////////////////////////
 
-// GET / - Home route redirects to /books
+// GET / - Home route redirects to /listOfBooks
 router.get("/", (req, res) => {
   console.log("DEBUG: Redirect to /books");
   res.redirect("/books");
@@ -25,16 +25,16 @@ router.get("/books", async (req, res) => {
 });
 
 // GET /books/new - Show the create new book form
-router.get("/books/new", async (req, res) => {
-  // const books = await Book.create();
-  console.log("DEBUG: " + req.body);
-  res.redirect("/books");
+router.get("/books/createbook", async (req, res) => {
+  console.log("DEBUG: You are in the /books/createbook route");
+  res.render("newBook", { Book, title: "Create New Book" });
 });
 
 // GET /books/:id - Show book detail form
 router.get("/books/:id", async (req, res) => {
   const book = await Book.findByPk(req.params.id);
   if (book) {
+    console.log("DEBUG: You are in the /books/:id route");
     res.render("updateBook", { book, title: book.title });
   } else {
     res.sendStatus(404);
@@ -46,6 +46,29 @@ router.get("/books/:id", async (req, res) => {
 /////////////////////////////
 
 // POST /books/new - Create a new book
+router.post("/books/:id", async (req, res) => {
+  const book = await Book.create(req.body);
+  res.redirect("/books/" + book.id);
+
+  // let book;
+  // try {
+  //   book = await Book.findByPk(req.params.id);
+  //   await book.update(req.body);
+  //   res.redirect("/books");
+  // } catch (error) {
+  //   if (error.name === "SequelizeValidationError") {
+  //     book = await Book.build(req.body);
+  //     book.id = req.params.id;
+  //     res.render("update-book", {
+  //       book,
+  //       errors: error.errors,
+  //       title: `${book.title}`,
+  //     });
+  //   } else {
+  //     throw error;
+  //   }
+  // }
+});
 
 // POST /books/:id - Update book info
 
