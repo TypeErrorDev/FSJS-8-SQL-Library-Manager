7e44aed0c5d1db0aa88733a39432699f0e639ea4
typeerrordev <matthew.pantel86@gmail.com>
2022-09-29 15:17:43 +0200

    ADD: fixed the /books hyperlinks, completed create operations, completed delete operations

--- routes/index.js
+++ routes/index.js
@@ -46,34 +46,41 @@ router.get("/books/:id", async (req, res) => {
 /////////////////////////////
 
 // POST /books/new - Create a new book
-router.post("/", async (req, res) => {});
-
-// POST /books/:id - Update book info
-router.post("/:id/edit", async (req, res) => {
+router.post("/books/new", async (req, res) => {
   let book;
   try {
-    book = await Book.findByPk(req.params.id);
-    if (book) {
-      await book.update(req.body);
-      res.redirect("/books/" + books.id);
-    } else {
-      res.sendStatus(404);
-    }
+    book = await Book.create(req.body);
+    res.redirect(`/books/${book.id}`);
   } catch (error) {
     if (error.name === "SequelizeValidationError") {
       book = await Book.build(req.body);
-      book.id = req.params.id; // make sure correct book gets updated
-      res.render("book/edit", {
-        book,
-        errors: error.errors,
-        title: "Edit book",
-      });
+      res.render("new-book", { book, errors: error.errors, title: "New Book" });
     } else {
       throw error;
     }
   }
 });
 
+// POST /books/:id - Update book info
+router.post("/books/:id", async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  if (book) {
+    await book.update(req.body);
+    res.redirect("/books");
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 // POST /books/:id/delete - Delete a book
+router.post("/books/:id/delete", async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  if (book) {
+    await book.destroy();
+    res.redirect("/books");
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 module.exports = router;
